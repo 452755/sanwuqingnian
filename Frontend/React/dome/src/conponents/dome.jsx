f9a536ecd141df55cb1dfb51063ff04eeb79171f
三无青年 <2955984057@qq.com>
2024-07-27 20:02:36 +0800

    提交React dome项目

---
+++ Frontend/React/dome/src/conponents/dome.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class dome extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+
+    
+    static propTypes = {
+        
+    }
+}
+
+
+export default dome;
