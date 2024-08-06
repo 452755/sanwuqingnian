33930807103e4d07e00ce73348a679a540cf9bef
郭靖 <2955984057@qq.com>
2024-08-06 13:58:36 +0800

    提交Android VideoToolbar 项目

---
+++ Android/VideoToolbar/app/src/main/java/com/sanwuqingnian/videotoolbar/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sanwuqingnian.videotoolbar.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sanwuqingnian.videotoolbar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
