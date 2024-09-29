5ffae4ba5bc0fcfea239e54a5bcd167295f6eb90
郭靖 <2955984057@qq.com>
2024-09-29 11:28:43 +0800

    修改 ConsoleTest/ConsoleTest/ 项目

--- C#/ConsoleTest/ConsoleTest/Program.cs
+++ C#/ConsoleTest/ConsoleTest/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
+
+
+public class test 
+{
+    public void test1() 
+    {
+        test2(() => { Console.WriteLine("fgsdh"); });
+    }
+
+    public void test2(Action action) 
+    {
+
+    }
+}
